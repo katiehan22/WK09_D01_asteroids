99386ffd62234a77363a35d7fc357a1fb7ae2d54
katiehan22 <katiehan22@gmail.com>
2022-10-03 16:02:13 -0700

    Add GameView

--- src/game.js
+++ src/game.js
@@ -1,26 +1,41 @@
 const Asteroid = require("./asteroid");
 
 function Game (){
-    const DIM_X = 500;
-    const DIM_Y = 500;
-    const NUM_ASTEROIDS = 10;
+    this.DIM_X = 500;
+    this.DIM_Y = 500;
+    this.NUM_ASTEROIDS = 10;
     this.asteroids = this.addAsteroids();
 }
 
 Game.prototype.addAsteroids = function(){
-    let array = []
-    for (let i = 0 ; i < NUM_ASTEROIDS ; i++){
+    let array = [];
+    for (let i = 0 ; i < this.NUM_ASTEROIDS ; i++){
         let object = {
-            pos: [this.randomPosition(0, DIM_X), this.randomPosition(0, DIM_Y)]
+            pos: [this.randomPosition(0, this.DIM_X), this.randomPosition(0, this.DIM_Y)]
         }
         let asteroid = new Asteroid(object);
         array.push(asteroid);
     };
-    return array
+    return array;
 }
 
 Game.prototype.randomPosition = function(min, max){
-    let min = Math.ceil(min);
-    let max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    let min1 = Math.ceil(min);
+    let max1 = Math.floor(max);
+    return Math.floor(Math.random() * (max1 - min1 + 1) + min1);
 }
+
+Game.prototype.draw = function(ctx) {
+    ctx.clearRect(0, 0, this.DIM_X, this.DIM_Y);
+    for(let i = 0; i < this.asteroids.length; i++) {
+        this.asteroids[i].draw(ctx);
+    }
+}
+
+Game.prototype.moveObjects = function() {
+    for (let i = 0; i < this.asteroids.length; i++) {
+        this.asteroids[i].move();
+    }
+}
+
+module.exports = Game;
--- src/index.js
+++ src/index.js
@@ -6,33 +6,39 @@ const ctx = canvas.getContext("2d");
 
 const MovingObject = require('./moving_object.js');
 const Asteroid = require('./asteroid.js');
+const Game = require('./game.js');
+const GameView = require('./game_view.js');
 window.MovingObject = MovingObject;
 
-const mo = new MovingObject({
-  pos: [250, 250],
-  vel: [10, 10],
-  radius: 100,
-  color: "#00FF00"
-});
+// const mo = new MovingObject({
+//   pos: [250, 250],
+//   vel: [10, 10],
+//   radius: 100,
+//   color: "#00FF00"
+// });
 
-mo.draw(ctx);
-mo.move();
-mo.draw(ctx);
+// mo.draw(ctx);
+// mo.move();
+// mo.draw(ctx);
 
-asteroid1 = new Asteroid({
-  pos: [100, 100]
-})
+// asteroid1 = new Asteroid({
+//   pos: [100, 100]
+// })
 
-asteroid1.draw(ctx);
-asteroid1.move();
-asteroid1.draw(ctx);
-console.log(asteroid1.vel);
+// asteroid1.draw(ctx);
+// asteroid1.move();
+// asteroid1.draw(ctx);
+// console.log(asteroid1.vel);
 
-asteroid2 = new Asteroid({
-  pos: [200, 200]
-})
+// asteroid2 = new Asteroid({
+//   pos: [200, 200]
+// })
 
-asteroid2.draw(ctx);
-asteroid2.move();
-asteroid2.draw(ctx);
-console.log(asteroid2.vel);
+// asteroid2.draw(ctx);
+// asteroid2.move();
+// asteroid2.draw(ctx);
+// console.log(asteroid2.vel);
+
+// let game = new Game();
+let gameView = new GameView(ctx);
+gameView.start();

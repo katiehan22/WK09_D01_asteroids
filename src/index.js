3e75ee82a6fff2a21fdc9638422a312e23312864
Connor Norton <connorn314@gmail.com>
2022-10-03 13:47:43 -0700

    add Moving Object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log("Webpack is working!");
+const canvas = document.getElementById("game-canvas")
+const ctx = canvas.getContext("2d")
+
+const MovingObject = require('./moving_object.js')
+window.MovingObject = MovingObject
---
+++ src/moving_object.js
@@ -0,0 +1,20 @@
+
+function MovingObject(object){
+    this.pos = object.pos;
+    this.vel = object.vel;
+    this.radius = object.radius;
+    this.color = object.color;
+}
+
+MovingObject.prototype.draw = function(ctx){
+    
+}
+
+
+
+
+
+
+
+
+module.exports = MovingObject;

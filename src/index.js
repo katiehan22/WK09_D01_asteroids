8eadb492d02e5967c158fbf9ad29867091ed2455
katiehan22 <katiehan22@gmail.com>
2022-10-03 14:04:59 -0700

    Add draw fuction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 console.log("Webpack is working!");
 const canvas = document.getElementById("game-canvas")
+canvas.width = 500;
+canvas.height = 500;
 const ctx = canvas.getContext("2d")
 
 const MovingObject = require('./moving_object.js')
 window.MovingObject = MovingObject
+
+const mo = new MovingObject({
+  pos: [250, 250],
+  vel: [10, 10],
+  radius: 100,
+  color: "#00FF00"
+});
+
+mo.draw(ctx);
+mo.move();
+mo.draw(ctx);
--- src/moving_object.js
+++ src/moving_object.js
@@ -7,7 +7,14 @@ function MovingObject(object){
 }
 
 MovingObject.prototype.draw = function(ctx){
-    
+    ctx.beginPath();
+    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI * 2, true);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+}
+
+MovingObject.prototype.move = function() {
+    this.pos = [this.pos[0] + this.vel[0], this.pos[1] + this.vel[1]];
 }
 
 
